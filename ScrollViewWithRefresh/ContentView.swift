ee1a1fc96c648c28cf162186ab6fdddd660683e9
Alien <cattla@outlook.com>
2020-11-20 22:39:56 +0800

    Initial Commit

---
+++ ScrollViewWithRefresh/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ScrollViewWithRefresh
+//
+//  Created by Alien Lee on 2020/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ScrollViewWithRefresh/ScrollViewWithRefreshApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewWithRefreshApp.swift
+//  ScrollViewWithRefresh
+//
+//  Created by Alien Lee on 2020/11/20.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewWithRefreshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
